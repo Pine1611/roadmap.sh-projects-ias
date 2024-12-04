6f6a0fbf8b2797f30ed9b6c23088bc08c82bb750
Pine1611 <iampine1611@gmail.com>
2024-12-04 19:02:47 +0700

    removed old folder BE in root

--- backend_projects/01-task-tracker/src/task-config.js
+++
@@ -1,5 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const { DATA_PATH, DATA_FILE } = process.env;
-export { DATA_PATH, DATA_FILE };
--- backend_projects/03-unit-converter/src/middlewares/notFound.mjs
+++
@@ -1,5 +0,0 @@
-import { createCustomError } from "../utils/customError.mjs";
-
-export const notFound = (_req, res, next) => {
-    return next(createCustomError("invalid_route", 404));
-};
